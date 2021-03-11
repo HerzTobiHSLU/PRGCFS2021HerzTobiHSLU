a91ddf0e29daeea3f36bbd308259225a25214138
prgc <tobias.herzog@stud.hslu.ch>
2021-03-11 19:16:01 +0100

    aufräumen

---
+++ 40_Exercises/02_HelloWorld/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World\n"); // \n fügt neue Zeile am Ende ein
+	
+	return 0;
+}
+
+
---
+++ 40_Exercises/03_DataTypes/03_datatypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main()
+{ 
+
+	printf("size of char: %ld\n", sizeof(char) );
+	printf("size of uchar: %ld\n", sizeof(unsigned char) );
+	printf("size of short: %ld\n", sizeof(short) );
+	printf("size of int: %ld\n", sizeof(int) );
+	printf("size of float: %ld\n", sizeof(float) );
+	
+	
+	return 0;
+}
