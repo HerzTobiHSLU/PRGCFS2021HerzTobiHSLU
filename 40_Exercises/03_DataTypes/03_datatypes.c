b53d31d5a573a9128c029ce99d1bff6fe11a1041
prgc <tobias.herzog@stud.hslu.ch>
2021-03-11 20:56:32 +0100

    n-ter versuch

--- 40_Exercises/03_DataTypes/03_datatypes.c
+++ 40_Exercises/03_DataTypes/03_datatypes.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <stdio.h> //weist programmierbefehle library zu
 
 
-int main()
+int main()  //Top Level, definiert die Ausgabe des Programmes als integer
 { 
 
-	printf("size of char: %ld\n", sizeof(char) );
+	printf("size of char: %ld\n", sizeof(char) ); //Printf sagt dem Programm, bilde auf terminal ab
 	printf("size of uchar: %ld\n", sizeof(unsigned char) );
 	printf("size of short: %ld\n", sizeof(short) );
 	printf("size of int: %ld\n", sizeof(int) );
