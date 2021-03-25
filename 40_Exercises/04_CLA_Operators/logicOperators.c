37a25f6c1b1efca2a61c533fb0fab3b98f3a02f5
prgc <tobias.herzog@stud.hslu.ch>
2021-03-25 20:28:28 +0100

    commit

--- 40_Exercises/04_CLA_Operators/logicOperators.c
+++ 40_Exercises/04_CLA_Operators/logicOperators.c
@@ -1,23 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+const int NUM_VALUES = 3;
 
 int main(int argc, char *argv[] )
 { 
-	if (argc !=3){ 
+	//falls NICHT 4 argumente erhalten, dann --> Block 7 bis 10
+	if ( argc !=NUM_VALUES + 1  ) // because program call is 
+	{ 
 		// input not valid
 		printf("Usage:\n"); 
-		printf("logicOperators Input1 Input2 \n");
+		printf("logicOperators Input1 Input2 Input3\n");
 	}
 	else
-	{ 	// input valid
-	
-		printf("Number of input arguments: %d\n", argc );
-		for( int i = 0; i < argc; i++ )
-		{ 
-		printf("Argument %d was %s\n", i, argv[i]);
-		};
-	}
-	return 0;
+	{ 	// input is valid  --> genau 1+3 argumente erhalten
+		int val1 = atoi(argv[1]);
+		int val2 = atoi(argv[2]);
+		int val3 = atoi(argv[3]);
+		int sum = 0;
+		float average = 0.0f;
+		
+		printf("%d, %d, %d\n", val1, val2, val3);
+		
+		if ( !(val1 > 0) || !(val2 > 0) || !(val3 > 0) )
+		{
+			// fehlerfall
+			printf("All inputs must be strictly greater than zero.\n");
+		}
+		else
+		{
+			//find the greatest	
+			if( val1 >= val2 && val1 >= val3 )
+			{
+				printf("%d is the greatest\n", val1);
+			}	
+			else if( val2 >= val1 && val2 >= val3 )
+			{
+				printf("%d is the greatest\n", val2);
+			}
+			else
+			{
+				printf("%d is the greatest\n", val3);
+			}
+			
+			// find the smallest
+			if( val1 <= val2 && val1 <= val3 )
+			{
+				printf("%d is the smallest\n", val1);
+			}	
+			else if( val2 <= val1 && val2 <= val3 )
+			{
+				printf("%d is the smallest\n", val2);
+			}
+			else
+			{
+				printf("%d is the smallest\n", val3);
+			}
+			
+			// compute the sum
+			sum = val1 + val2 + val3;
+			printf("The sum is: %d\n", sum);
+			
+			// compute the average
+			average = sum/NUM_VALUES;
+			printf("The average is: %.2f\n", average);
+		
+		}
+	}	
 }
+		
+			
+				
 
 
 
